d952765b2c1688e3007c826e8c04b58b7794887e
danny.c.bowman@gmail.com <danny.c.bowman@gmail.com@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-11 02:57:42 +0000

    This is the final version of hht 2.0.0, ready for submission to CRAN. Need to upload vignettes and add to DESCRIPTION Worked on vignettes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hht@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/vignettes/make_vignettes.sh
@@ -0,0 +1,8 @@
+R CMD Sweave interesting_signals.Rnw
+R CMD pdflatex interesting_signals.tex 
+R CMD pdflatex interesting_signals.tex 
+
+R CMD Sweave eemd_examples.Rnw
+R CMD pdflatex eemd_examples.tex
+R CMD pdflatex eemd_examples.tex
+R CMD pdflatex eemd_examples.tex
